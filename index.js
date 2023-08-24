8a1b9ca77bc0376a29d63f0e0d815a65a15f284a
Dave <d.ruocco1987@googlemail.com>
2023-08-24 14:10:21 +0100

    Sets up connection with MongoDB database, app endpoints working

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -12,8 +13,14 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model("Blog", blogSchema);
 
-const mongoUrl = "mongodb://localhost/bloglist";
-mongoose.connect(mongoUrl);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB", error.message);
+  });
 
 app.use(cors());
 app.use(express.json());
