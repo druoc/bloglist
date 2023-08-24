00a7aea3f94da7d2ee32394087bba4f6bb81f09f
Dave <d.ruocco1987@googlemail.com>
2023-08-24 15:02:27 +0100

    Refactors endpoint functionality into model and controllers

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const { infoLogger, errorLogger } = require("./utils/logger");
 const { PORT, MONGODB_URI } = require("./utils/config");
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
+const blogsRouter = require("./controllers/blogs");
 
 mongoose
   .connect(MONGODB_URI)
@@ -21,25 +12,14 @@ mongoose
     infoLogger("connected to MongoDB");
   })
   .catch((error) => {
-    infoLogger("error connecting to MongoDB", error.message);
+    infoLogger("error connecting to MongoDB");
+    errorLogger(error.message);
   });
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", blogsRouter);
 
 app.listen(PORT, () => {
   infoLogger(`Server running on port ${PORT}`);
