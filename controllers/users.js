2b5abc9fcb73c90a12c6a9d8a9bbc7f51901c4d7
Dave <d.ruocco1987@googlemail.com>
2023-09-04 14:15:33 +0100

    Adds username and password validation and errorhandling to api/users endpoint

--- controllers/users.js
+++ controllers/users.js
@@ -4,6 +4,13 @@ const User = require("../models/user");
 
 userRouter.post("/", async (req, res) => {
   const { username, name, password } = req.body;
+  if (!username || username.length < 3) {
+    return res.status(400).json({ error: "a username is required" });
+  }
+
+  if (!password || password.length < 3) {
+    return res.status(400).json({ error: "a password is required" });
+  }
   //password salting
   const passwordHash = await bcrypt.hash(password, 10);
 
